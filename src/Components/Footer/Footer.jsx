f7c9916ac9084fc0f7f6765c19621ca7ac6e2f11
Fatema-tuj-Jahura <fatema.tuj.jahura@g.bracu.ac.bd>
2025-02-10 07:39:13 +0600

    Added footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,19 @@
-import { FaLinkedin, FaTwitter, FaPeace, FaGithub } from "react-icons/fa";
+import { FaLinkedin, FaTwitter, FaFacebookF, FaGithub } from "react-icons/fa";
+import { MdVolunteerActivism } from "react-icons/md";
 
 const Footer = () => {
   return (
     <footer className="bg-[#6A7F72] text-white">
       {/* Upper Section */}
-      <div className="container mx-auto py-10 px-6 grid md:grid-cols-4 gap-8">
+      <div className="container mx-auto py-10 px-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-8">
+        <div>
+        <div className="flex items-center space-x-1">
+          <span className="text-4xl font-extrabold text-[#EC9C85]">
+            <MdVolunteerActivism />
+          </span>
+          <span className="text-2xl font-bold text-[#FDFBCF]">Join Hands</span>
+        </div>
+        </div>
         <div>
           <h2 className="font-bold text-lg">SOLUTIONS</h2>
           <ul className="mt-4 space-y-2">
@@ -45,11 +54,13 @@ const Footer = () => {
 
       {/* Lower Section */}
       <div className="bg-[#95AA9B] text-white py-6 text-center flex flex-col md:flex-row justify-between items-center px-6">
-        <p className="text-sm">&copy; 2025 YourCompany - All Rights Reserved</p>
+      <p className="text-sm flex items-center">
+       &copy; 2025 <span className="mx-1"><MdVolunteerActivism /></span> Join Hands - All Rights Reserved
+      </p>
         <div className="flex space-x-4 mt-4 md:mt-0">
           <FaLinkedin size={20} />
           <FaTwitter size={20} />
-          <FaPeace size={20} />
+          <FaFacebookF size={20} />
           <FaGithub size={20} />
         </div>
       </div>
